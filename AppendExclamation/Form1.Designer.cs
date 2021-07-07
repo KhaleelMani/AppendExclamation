e8e8ec03ba7d2e907a984910504f0472eaca01b6
User <User@MSI>
2021-07-07 20:44:59 +0300

    Add project files.

---
+++ AppendExclamation/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace AppendExclamation
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.SelectFile = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Save = new System.Windows.Forms.Button();
+            this.SelectedName = new System.Windows.Forms.Label();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // SelectFile
+            // 
+            this.SelectFile.Location = new System.Drawing.Point(12, 169);
+            this.SelectFile.Name = "SelectFile";
+            this.SelectFile.Size = new System.Drawing.Size(75, 23);
+            this.SelectFile.TabIndex = 0;
+            this.SelectFile.Text = "Select File";
+            this.SelectFile.UseVisualStyleBackColor = true;
+            this.SelectFile.Click += new System.EventHandler(this.SelectFile_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(503, 109);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Save
+            // 
+            this.Save.Location = new System.Drawing.Point(440, 169);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(75, 23);
+            this.Save.TabIndex = 2;
+            this.Save.Text = "Save";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // SelectedName
+            // 
+            this.SelectedName.AutoSize = true;
+            this.SelectedName.Location = new System.Drawing.Point(216, 173);
+            this.SelectedName.Name = "SelectedName";
+            this.SelectedName.Size = new System.Drawing.Size(78, 15);
+            this.SelectedName.TabIndex = 3;
+            this.SelectedName.Text = "Selected File :";
+            this.SelectedName.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(527, 204);
+            this.Controls.Add(this.SelectedName);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.SelectFile);
+            this.Name = "Form1";
+            this.Text = "Append Exclamation";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SelectFile;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.Label SelectedName;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+    }
+}
+
---
+++ AppendExclamation/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace AppendExclamation
+{
+    public partial class Form1 : Form
+    {
+        OpenFileDialog openfile = new OpenFileDialog(); //Initialize Open File dialogue
+        string line = "";                                                  
+        List<String> list = new List<String>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            openfile.Filter = "Text Files (.txt)| *.txt"; //Setting up the Open File Dialogue Filter on Form Load
+        }
+
+        private void SelectFile_Click(object sender, EventArgs e)
+        {
+            if (openfile.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader sr = new StreamReader(openfile.FileName); // Initializing Stream Reader to Read the Text from the Selected file
+                SelectedName.Text = SelectedName.Text + Path.GetFileNameWithoutExtension(openfile.FileName); //Display the Selected File Name on the Label
+                while (line != null)
+                {
+                    line = sr.ReadLine();
+                    if (line != null)
+                    {
+                        list.Add(line); //Adding Each Line the Stream Reader Reads From The File to a List
+                    }
+                }
+                sr.Close(); //Closing the Stream Reader
+            }
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+
+
+            SaveFileDialog sfg = new SaveFileDialog(); //Initializing Save File Dialogue 
+            sfg.Filter = "Text files (*.txt)|*.txt"; //Setting Up the Saving Format
+            if (sfg.ShowDialog() == DialogResult.OK)
+            {
+                var path = sfg.FileName;
+                StreamWriter sw = new StreamWriter(path, true); //Initializing the Stream Writer to Create a File in the Path Chosen by Save File Dialogue
+                foreach (String s in list)
+                {
+
+                    sw.WriteLine(s + "!"); //Appending an Exclamation Point to each Line in the List Then Typing the Lines in the Created File
+                }
+                sw.Close(); //Closing the Stream Writer
+                MessageBox.Show("File Created Successfully"); //Showing a Message to Notify the User When the File is Created
+            }
+
+
+        }
+
+
+
+    }
+}
